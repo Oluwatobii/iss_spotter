98c9e4fcbd86067ee69084fd240092d582427e37
Oluwatobii <bellotobi10@gmail.com>
2020-07-02 22:54:54 +0000

    ISS Spotter II - Fetching Geo Coordinates

--- iss.js
+++ iss.js
@@ -9,7 +9,7 @@
 
 const request = require("request");
 
-const fetchMyIP = function (callback) {
+const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
   const URL = "https://api.ipify.org/?format=json";
   request.get(URL, (error, response, body) => {
@@ -28,9 +28,32 @@ const fetchMyIP = function (callback) {
     const ip = JSON.parse(body).ip;
     callback(null, ip);
 
-    console.log("error:", error); // Print the error if one occurred
-    console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+    //console.log("error:", error); // Print the error if one occurred
+    //console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  const URL = "https://ipvigilante.com/json/";
+  request(`${URL}${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(
+        Error(
+          `Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`
+        ),
+        null
+      );
+      return;
+    }
+
+    const { latitude, longitude } = JSON.parse(body).data;
+
+    callback(null, { latitude, longitude });
+  });
+};
+module.exports = { fetchMyIP, fetchCoordsByIP };

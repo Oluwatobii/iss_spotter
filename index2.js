ecde2f740580f9e95710d6fdb82fa4f7680e9b35
Oluwatobii <bellotobi10@gmail.com>
2020-07-03 01:30:00 +0000

    ISS Spotter with Promise

---
+++ index2.js
@@ -0,0 +1,20 @@
+const { nextISSTimesForMyLocation } = require("./iss_promised");
+
+const printPassTimes = function (passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(
+      `The International Space Station passes your current location at ${datetime} for ${duration} seconds!`
+    );
+  }
+};
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
